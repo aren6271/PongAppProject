9f27e50995aa2b68e051108b91c905c38efd6877
Andy Ren <aren6271@gmail.com>
2022-10-11 01:15:52 -0700

    Completed Wall and Bat Collision
    WIP: Sounds, Rotation, Limiting Speeds until higher scores.

--- src/PongApp.java
+++ src/PongApp.java
@@ -23,7 +23,9 @@ import java.util.Set;
 
 public class PongApp extends Application{
     Point2D size = new Point2D(900, 1200);
+    final static double TAU = Math.PI * 2;
     Set<KeyCode> keysDown = new HashSet<>();
+
     int key(KeyCode k) {
         return keysDown.contains(k) ? 1 : 0;
     }
@@ -65,14 +67,6 @@ public class PongApp extends Application{
         ball.setStroke(Color.BLUE);
         ball.setStrokeWidth(3);
 
-            // Make ball fall on spawn
-        double bAngle = 0;
-        double bOmega = 0;
-        Point2D bv = PhysicsObject.vecAngle(Math.PI + bAngle + PhysicsObject.rand(-0.2,0.2), PhysicsObject.rand(50, 100));
-
-        Translate translate = new Translate();
-
-
         Group gGame = new Group();
         Group gBat = new Group(bat);
         Group gBall = new Group(ball);
@@ -113,11 +107,20 @@ public class PongApp extends Application{
             }
         });
 
+
         /* SETUP */
         AnimationTimer loop = new AnimationTimer() {
             double old = -1;
             double elapsedTime = 0;
             int score = 0;
+
+            double horzBall = 0;
+            double vertBall = 6;
+
+            double horzBat = 0;
+            double oldBatPos = bat.getX();
+
+
             public void handle(long nano) {
                 if (old < 0) old = nano;
                 double delta = (nano - old) / 1e9; // Divide by 1e9 to put time into Nanosecs
@@ -125,12 +128,19 @@ public class PongApp extends Application{
                 old = nano;
                 elapsedTime += delta;
 
+                // Make this dynamic not just a number
+                // Just for testing
+                // Divide Delta by Framerate?
+                horzBat = (bat.getX()-oldBatPos)/delta/144;
+                oldBatPos = bat.getX();
+
+                System.out.println(horzBat);
 
                 fpsLabel.setText(String.format("%.2f %.2f", 1 / delta, elapsedTime)); // 1/delta = frames per sec
                 scoreLabel.setText(Integer.toString(score));
                 /* GAME LOOP */
 
-                System.out.println(keysDown);
+                //System.out.println(keysDown);
 
                 if (keysDown.contains(KeyCode.I)) {
                     fpsLabel.setVisible(true);
@@ -143,27 +153,53 @@ public class PongApp extends Application{
                     direction = 1;
                 }
 
-                /* Bat Movement */
-
-
-
-                //bat.update(delta, 0, key(KeyCode.D));
-
-                //double rot = 4 * (key(KeyCode.D) - key(KeyCode.A));
-                //ball.update(delta, rot, key(KeyCode.W));
-
                 /* BALL Movement */
-                translate.setY(5);
-                ball.getTransforms().addAll(translate);
+                Point2D pBall = new Point2D(ball.getX(), ball.getY());
 
-                if (leavingBounds(ball, bounds)) {
+                //System.out.println((pBall.add(vBall)).getY());
+
+                ball.setX(pBall.add(horzBall, vertBall).getX());
+                ball.setY(pBall.add(horzBall, vertBall).getY());
+
+                if (leavingBounds(ball, bounds) == 0) {
                     System.out.println("GAME OVER");
-                    ball.setX(Math.random() * (scene.getWidth() - 50) + 50);
-                    ball.getTransforms().clear();
+//                    ball.getTransforms().clear();
+                    ball.setX(Math.random() * (scene.getWidth() - 100));
+                    ball.setY(Math.random() * (50));
+                    horzBall = 0;
+                    vertBall = 6;
+                    score = 0;
+                }
+                if (leavingBounds(ball, bounds) == 1) {
+                    score+=1;
+                    vertBall = vertBall *-1;
+                }
+                if (leavingBounds(ball, bounds) == 2 ) {
+                    score+=1;
+                    horzBall = horzBall *-1;
+                }
+                if (leavingBounds(ball, bounds) == 3) {
+                    score+=1;
+                    horzBall = horzBall *-1;
                 }
 
                 // If the ball hits the bat
-//                if (ball.intersect(ball, bat).getBoundsInLocal().isEmpty())
+                if (!ball.intersect(ball, bat).getBoundsInLocal().isEmpty()) {
+                    System.out.println("HIT");
+                    score+=1;
+                    Point2D hitVelocity = new Point2D(horzBall, vertBall).add(horzBat, 0);
+                    horzBall = hitVelocity.getX();
+                    vertBall = hitVelocity.getY() *-1;
+
+                    //vBall = Point2D(vBall.getX(), vBall.get)
+                    //vertBall[0] = vertBall[0] *-1;
+//                    ball.getTransforms().addAll(
+//                            new Translate(vBall.getX(), vBall.getY())
+//                            new Rotate(Math.toDegrees(theta))
+//                    );
+
+                }
+
 
                 //System.out.println(ball.intersects(bat.getBoundsInLocal()));
             }
@@ -174,131 +210,45 @@ public class PongApp extends Application{
         stage.show();
     }
 
-    public boolean leavingBounds(Rectangle ball, Rectangle bounds) {
+    public int leavingBounds(Rectangle ball, Rectangle bounds) {
         if (!ball.intersect(ball, bounds).getBoundsInLocal().isEmpty()) {
-            return false;
+            // ENUM This
+            if (ball.getY() < 0 ) {
+                return 1; // Top
+            } else if (ball.getX() + ball.getWidth() > bounds.getWidth()) {
+                return 2; // Right
+            } else if (ball.getX() < 0) {
+                return 3; // Left
+            }
+        } else {
+            if (ball.getY() > bounds.getHeight()) {
+                return 0; // Bottom
+            }
         }
-        return true;
+        return -1;
     }
+
+//    public Point2D vecAngle(double angle, double mag) {
+//        return new Point2D(Math.cos(angle), Math.sin(angle)).multiply(mag);
+//    }
+//
+//    public double rand(double min, double max) {
+//        return Math.random() * (max - min) + min;
+//    }
+
+//    public void update(double delta, Point2D p, Point2D v, double theta, double omega, Rectangle transform) {
+//        p = p.add(v.multiply(delta));
+//        theta = (theta + omega * delta) % TAU;
+//
+//        transform.getTransforms().clear();
+//        transform.getTransforms().addAll(
+//                new Translate(p.getX(), p.getY()),
+//                new Rotate(Math.toDegrees(theta))
+//        );
+//    }
 
     public static void main(String[] args) {
         launch(args);
     }
 }
 
-abstract class PhysicsObject {
-    Point2D p, v;
-    double theta, omega; //angle and velocity of object
-    Group root, transform;
-    boolean alive;
-
-    final static double TAU = Math.PI * 2;
-
-    public PhysicsObject(Group parent, Point2D p0, Point2D v0, double theta0, double omega0) {
-        root = new Group();
-        transform = new Group();
-        root.getChildren().add(transform);
-        parent.getChildren().add(root);
-
-        p = p0;
-        v = v0;
-        theta = theta0;
-        omega = omega0;
-        alive = true;
-    }
-
-    public void update(double delta) {
-        p = p.add(v.multiply(delta));
-        theta = (theta + omega * delta) % TAU;
-
-        transform.getTransforms().clear();
-        transform.getTransforms().addAll(
-                new Translate(p.getX(), p.getY()), //transform then rotate (order matters)
-                new Rotate(Math.toDegrees(theta))
-        );
-    }
-
-    public void destroy(Group parent) {
-        parent.getChildren().remove(root);
-        alive = false;
-    }
-
-    abstract Shape getShapeBounds();
-
-    public boolean intersects(PhysicsObject po) {
-        return alive && po.alive &&
-                !Shape.intersect(getShapeBounds(), po.getShapeBounds())
-                .getBoundsInLocal().isEmpty(); // Checks if the 2 objects are colliding
-    }
-
-    static Point2D vecAngle(double angle, double mag) {
-        return new Point2D(Math.cos(angle), Math.sin(angle)).multiply(mag);
-    }
-
-    static double rand(double min, double max) {
-        return Math.random() * (max - min) + min;
-    }
-}
-
-//class Ball extends PhysicsObject {
-//    double thrust = 150;
-//    Polygon pgon;
-//
-//    public Ball(Group parent, Point2D p) {
-//        super(parent, p, Point2D.ZERO, 0, 0);
-//
-//        pgon = new Polygon(0.7, 0.7, 0.7, -0.7, -0.7, -0.7, -0.7, 0.7);
-//
-//        transform.getChildren().add(pgon);
-//
-//        pgon.setStroke(Color.rgb(255, 0, 0));
-//        pgon.setStrokeWidth(0.1);
-//        pgon.getTransforms().add(new Scale(30,30));
-//
-//        update(0,0,0);
-//    }
-//
-//    public void update(double delta, double omega, double throttle) {
-//        if (throttle != 0) {
-//            Point2D acc = vecAngle(theta, thrust * throttle);
-//            v = v.add(acc.multiply(delta));
-//        } else {
-//            v = v.multiply(1 - 0.2 * delta);
-//        }
-//
-//        this.omega = omega;
-//        super.update(delta);
-//    }
-//
-//    public Shape getShapeBounds() { return pgon; }
-//}
-//
-//class Bat extends PhysicsObject {
-//    double thrust = 150;
-//    Rectangle rect;
-//
-//    public Bat(Group parent, Point2D p) {
-//        super(parent, p, Point2D.ZERO, 0, 0);
-//
-//        rect = new Rectangle(100, 10, Color.RED);
-//        rect.setStroke(Color.RED);
-//        rect.setStrokeWidth(3);
-//
-//        transform.getChildren().add(rect);
-//
-//        update(0);
-//    }
-//
-//    public void update(double delta, double omega, double throttle) {
-//        // Able to move the bat?
-//        if (throttle != 0) {
-//            Point2D acc = vecAngle(theta, thrust * throttle);
-//            v = v.add(acc.multiply(delta));
-//        } else {
-//            v = v.multiply(1 - 0.2 * delta);
-//        }
-//        this.omega = omega;
-//        super.update(delta);
-//    }
-//    public Shape getShapeBounds() { return rect; }
-//}
